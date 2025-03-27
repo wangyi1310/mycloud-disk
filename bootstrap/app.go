812e19805d23caa2d1c9fea8bf153a9b977270de
ottoypwang <ottoypwang@tencent.com>
2025-03-27 13:24:39 +0800

    feat: 增加配置文件处理

---
+++ bootstrap/app.go
@@ -0,0 +1,18 @@
+package bootstrap
+
+import "fmt"
+
+func InitApplication() {
+	fmt.Print(`
+   ___ _                 _                    
+  / __\ | ___  _   _  __| |_ __ _____   _____ 
+ / /  | |/ _ \| | | |/ _  | '__/ _ \ \ / / _ \	
+/ /___| | (_) | |_| | (_| | | |  __/\ V /  __/
+\____/|_|\___/ \__,_|\__,_|_|  \___| \_/ \___|
+
+   V` + conf.BackendVersion + `  Commit #` + conf.LastCommit + `  Pro=` + conf.IsPro + `
+================================================
+
+`)
+	go CheckUpdate()
+}
--- main.go
+++ main.go
@@ -1,6 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+
+	"github.com/wangyi1310/mycloud-disk/pkg/util"
+)
+
+var (
+	isEject    bool
+	confPath   string
+	scriptName string
+)
+
+func init() {
+	flag.StringVar(&confPath, "c", util.RelativePath("conf.ini"), "Path to the config file.")
+	flag.Parse()
+}
 
 func main() {
 	fmt.Println("Hello World")
